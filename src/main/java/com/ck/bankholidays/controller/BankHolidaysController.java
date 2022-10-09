ce75136babed039f9623a925a0264f096fecb87f
Chandrakumar Murugesan <thisisckm@gmail.com>
2022-10-09 21:58:11 +0000

    Holiday api service

--- src/main/java/com/ck/bankholidays/controller/BankHolidaysController.java
+++ src/main/java/com/ck/bankholidays/controller/BankHolidaysController.java
@@ -1,23 +1,40 @@
 package com.ck.bankholidays.controller;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ck.bankholidays.data.Holiday;
+import com.ck.bankholidays.data.HolidayRepository;
+
+
 @RestController
 public class BankHolidaysController {
-    
-    @GetMapping("/isholiday")
-    public String isHoliday(@RequestParam String date) {
+
+    @Autowired
+	private HolidayRepository repository;
+
+    @GetMapping("/holiday")
+    public List<Holiday> isHoliday(@RequestParam Optional<String> date) {
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
-            LocalDate localDate = LocalDate.parse(date, formatter);
-            return localDate.toString();
+            if (date.isPresent()) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
+                LocalDate localDate = LocalDate.parse(date.get(), formatter);
+                return List.of(repository.findByDate(Date.valueOf(localDate)));
+            }
+            else {
+                return repository.findAllUpComingHoliday(new Date(System.currentTimeMillis()));
+            }
         }
         catch (Exception ex) {
-            return "";
+            return List.of();
         }
     }
 }
