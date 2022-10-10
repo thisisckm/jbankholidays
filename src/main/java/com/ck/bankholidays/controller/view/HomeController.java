8d89dc1a5c73dc483954d5d1f39e8a71a0d764d9
Chandrakumar Murugesan <thisisckm@gmail.com>
2022-10-10 21:02:32 +0000

    Upcoming holidays count based on future holidays

--- src/main/java/com/ck/bankholidays/controller/view/HomeController.java
+++ src/main/java/com/ck/bankholidays/controller/view/HomeController.java
@@ -1,6 +1,7 @@
 package com.ck.bankholidays.controller.view;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ck.bankholidays.data.Holiday;
 import com.ck.bankholidays.data.HolidayRepository;
 
 @Controller
@@ -19,8 +21,18 @@ public class HomeController {
 
     @GetMapping("/")
     public String template(Model model) {
-        model.addAttribute("holidays", 
-            repository.findAllUpComingHoliday(new Date(System.currentTimeMillis())).subList(0, 3));
+
+        List<Holiday> upcomingHoliday = repository.findAllUpComingHoliday(new Date(System.currentTimeMillis())); 
+
+        int endIndex = 5;
+        if (upcomingHoliday.size() < 5) {
+            endIndex = upcomingHoliday.size() - 1;
+        }
+
+        upcomingHoliday = upcomingHoliday.subList(0, endIndex);
+
+        model.addAttribute("holidays", upcomingHoliday);
         return "home";
+        
     }
 }
